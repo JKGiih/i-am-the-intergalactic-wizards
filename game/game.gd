0d4586092a87dd16bb9755b2714f346fd7b52c2e
J. K. Giih <jkgiih@nervoushorse.com>
2017-06-29 19:37:20 +0300

    Background, alien and wizard as instances

--- game/game.gd
+++ game/game.gd
@@ -1,7 +1,13 @@
 extends Panel
 
+onready var wizard = preload("res://wizard.xml")
+onready var alien = preload("res://alien.xml")
+
 func _ready():
-	pass
+	var w = wizard.instance()
+	var a = alien.instance()
+	add_child(w)
+	add_child(a)
 
 func _on_goto_scene_pressed():
         get_node("/root/global").goto_scene("res://gameover.xml")
