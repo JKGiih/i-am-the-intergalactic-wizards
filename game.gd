691ffdafa2d3ab2fb5f0819b5c4041195d1bed19
J. K. Giih <jkgiih@nervoushorse.com>
2017-07-01 19:38:47 +0300

    Add license information

--- game.gd
+++ game.gd
@@ -1,3 +1,26 @@
+##
+##  I AM THE INTER-GALACTIC WIZARDS
+##
+##  Copyright 2017 Lasse Pouru
+##
+##  ====================================================================
+##
+##  This file is part of I Am Inter-Galactic Wizards.
+##
+##  I Am Inter-Galactic Wizards is free software: you can redistribute it
+##  and/or modify it under the terms of the GNU General Public License as
+##  published by the Free Software Foundation, either version 3 of the
+##  License, or (at your option) any later version.
+##
+##  I Am Inter-Galactic Wizards is distributed in the hope that it will be
+##  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with I Am Inter-Galactic Wizards. If not, see
+##  <http://www.gnu.org/licenses/>.
+
 extends Panel
 
 onready var wizard_player = preload("res://wizard_player.xml")
